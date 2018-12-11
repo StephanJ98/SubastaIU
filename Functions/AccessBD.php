01bbf2b71df68ed54dc4cd97601ee87eb4ab9647
Stéphan <diego.jeandon@gmail.com>
2018-12-11 11:52:00 +0100

    Codificacion de las Functions Falta: 	-Rellenar los datos en AccessBD.php para nuestra BD. 	-Comprobar si hay fallos. Hecho: 	-Rellenado el codigo en la mayoria de loas Functions. 	-Comentarios hechos.

---
+++ Functions/AccessBD.php
@@ -0,0 +1,28 @@
+<?php
+/*
+	Autor: 	GUI
+	Fecha de creación: 11/12/2018 
+	Función: Realiza la conexión a la base de datos. Es el único lugar donde se definen los parametros de conexión 
+			 a la bd
+*/
+function ConectarBD() //declaración de funcion
+	{
+	    $mysqli = new mysqli("", "", "", ""); //maquina, user, pass, bd
+		if ($mysqli->connect_errno) {
+			echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+			return false;
+		}
+		return $mysqli;
+	}
+
+
+
+
+/*Adaptar a PDO Y A NUESTRA BD*/
+
+
+
+?>
+
+
+
---
+++ Functions/Authentication.php
@@ -0,0 +1,17 @@
+<?php
+/*
+	Autor:	GUI
+	Fecha de creación: 11/12/2018 
+	Función: Esta función valida si existe la variable de session login. Si no existe redirige a la pagina de login.
+			 Si existe comprueba si el usuario tiene permisos para ejecutar la accion de ese controlador.
+*/
+function IsAuthenticated(){
+
+	if (!isset($_SESSION['login'])){
+		return false;
+	}
+	else{
+		return true;
+	}
+}
+?>
