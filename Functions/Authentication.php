d30ef71c3bf00a823b2f892e51113e3539cd4862
Stéphan <diego.jeandon@gmail.com>
2018-12-13 00:39:17 +0100

    Creanción del archivo USUARIOS_Model.php Necesita: 	-Verificación.

--- Functions/Authentication.php
+++ Functions/Authentication.php
@@ -6,7 +6,6 @@
 			 Si existe comprueba si el usuario tiene permisos para ejecutar la accion de ese controlador.
 */
 function IsAuthenticated(){
-
 	if (!isset($_SESSION['login'])){
 		return false;
 	}
